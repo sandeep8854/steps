cac3fd27030a9b67bd603a8a650644ed5d05779f
Sandeep Prajapati <sandeep.prajapati.23.2@mountblue.tech>
2023-07-29 22:49:00 +0530

    adding all file which are created to step app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStepFunc] = useState(1);
+  // let step = 3;
+  const [isOpen, setIsOpen] = useState(true);
+  console.log(isOpen);
+
+  function handlePrevious() {
+    if (step > 1) setStepFunc((currentStep) => currentStep - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      setStepFunc((currentStep) => currentStep + 1);
+      // setStepFunc((currentStep) => currentStep + 1);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <Button bgColor="#a21caf" textColor="#fff" onClick={handlePrevious}>
+              Previous
+              <span>👈</span>
+            </Button>
+
+            <Button bgColor="#a21caf" textColor="#fff" onClick={handleNext}>
+              Next<span>👉</span>
+            </Button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
-export default App;
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
